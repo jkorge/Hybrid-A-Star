9e9ab3a1f4c74a99c683af2c74ec049caade36e6
James Korge <jk5128@nyu.edu>
2018-10-27 11:30:05 -0400

    First attempt at adding a heuristic to Hybrid A* search

---
+++ build/CMakeFiles/hybridAStar.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/James/Desktop/carnd/Term 3 - Path Planning & Concentrations/Lectures/5_TrajectoryGeneration/hybrid_a_star/src/hybrid_breadth_first.cpp" "/mnt/c/Users/James/Desktop/carnd/Term 3 - Path Planning & Concentrations/Lectures/5_TrajectoryGeneration/hybrid_a_star/build/CMakeFiles/hybridAStar.dir/src/hybrid_breadth_first.cpp.o"
+  "/mnt/c/Users/James/Desktop/carnd/Term 3 - Path Planning & Concentrations/Lectures/5_TrajectoryGeneration/hybrid_a_star/src/main.cpp" "/mnt/c/Users/James/Desktop/carnd/Term 3 - Path Planning & Concentrations/Lectures/5_TrajectoryGeneration/hybrid_a_star/build/CMakeFiles/hybridAStar.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/hybrid_breadth_first.h
@@ -0,0 +1,65 @@
+#ifndef HYBRID_BREADTH_FIRST_H_
+#define HYBRID_BREADTH_FIRST_H_
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+class HBF {
+public:
+
+	int NUM_THETA_CELLS = 90;
+	double SPEED = 1.45;
+	double LENGTH = 0.5;
+
+	struct maze_s {
+	
+		int g;	// iteration
+		double f;	// heuristic
+		double x;
+		double y;
+		double theta;
+	};
+
+	struct maze_path {
+	
+		vector< vector< vector<int> > > closed;
+		vector< vector< vector<maze_s> > > came_from;
+		maze_s final;
+
+	};
+
+
+	/**
+  	* Constructor
+  	*/
+ 	HBF();
+
+	/**
+ 	* Destructor
+ 	*/
+ 	virtual ~HBF();
+
+ 	
+ 	int theta_to_stack_number(double theta);
+
+  int idx(double float_num);
+
+  vector<maze_s> expand(maze_s state, vector<int> goal);
+
+  maze_path search(vector< vector<int> > grid, vector<double> start, vector<int> goal);
+
+  vector<maze_s> reconstruct_path(vector< vector< vector<maze_s> > > came_from, vector<double> start, HBF::maze_s final);
+
+  double heuristic(double x, double y, vector<int> goal);
+
+  bool compare_states(HBF::maze_s a, HBF::maze_s b);
+  	
+
+};
+
+#endif
